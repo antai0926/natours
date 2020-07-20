dde2c54a13afa525d2b42e7224f59c09c75f3736
Brian Li <antai0926@gmail.com>
2020-07-20 23:28:05 +0800

    fix checkout success but not create bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -50,13 +50,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const userId = (await User.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
-  await Booking.create({ tour, userId, price });
+  await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
-  console.log('I am in webhookCheckout!');
   const signature = req.headers['stripe-signature'];
   let event;
   try {
